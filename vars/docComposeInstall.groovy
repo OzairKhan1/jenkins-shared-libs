1a765924c8efd7a5019d49cf7401dbc37be14492
Ozair Khan <ozairk048@gmail.com>
2025-09-26 16:39:04 +0500

    Update docComposeInstall.groovy

--- vars/docComposeInstall.groovy
+++ vars/docComposeInstall.groovy
@@ -1,10 +1,12 @@
 def call(String version = "1.29.2") {
-  echo "Installing docker-compose version: ${version}"
-sh """
-            set -e
-            sudo curl -L "https://github.com/docker/compose/releases/download/${version}/docker-compose-$(uname -s)-$(uname -m)" \
-                -o /usr/local/bin/docker-compose
-            sudo chmod +x /usr/local/bin/docker-compose
-        """
-   sh "docker-compose --version"
+    echo "Installing docker-compose version: ${version}"
+
+    sh """
+        set -e
+        sudo curl -L "https://github.com/docker/compose/releases/download/${version}/docker-compose-$(uname -s)-$(uname -m)" \
+            -o /usr/local/bin/docker-compose
+        sudo chmod +x /usr/local/bin/docker-compose
+    """
+
+    sh "docker-compose --version"
 }
